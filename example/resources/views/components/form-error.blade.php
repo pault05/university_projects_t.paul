459138ca0652f5a14a2883292c2421d3bab20892
pault05 <147524535+pault05@users.noreply.github.com>
2025-03-31 14:28:34 +0300

    Delete example directory

--- example/resources/views/components/form-error.blade.php
+++
@@ -1,4 +0,0 @@
-@props(['name'])
-@error($name)
-    <p class="text-xs text-red-500 italic font-bold"> {{$message}} </p>
-@enderror
--- example/resources/views/home.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Home Page
-    </x-slot:heading>
-    <iframe src="https://free.timeanddate.com/clock/i9gf2ur8/n2360/tlro32/fn12/pd2/tt0/tw1/tm3/ta1/tb4" frameborder="0" width="120" height="31"></iframe>
-</x-layout>
